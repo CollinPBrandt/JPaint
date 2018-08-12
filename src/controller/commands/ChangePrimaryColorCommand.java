aa3dd35c1ad109d7a32491998c4e76a3ea50b96b
Collin <Collin.brandt1@gmail.com>
2018-08-11 19:02:02 -0500

    Commented change primary and change secondary color commands

--- src/controller/commands/ChangePrimaryColorCommand.java
+++ src/controller/commands/ChangePrimaryColorCommand.java
@@ -20,16 +20,24 @@ public class ChangePrimaryColorCommand implements ICommand, IUndoable {
 
     @Override
     public void execute() {
+        changePrimaryColor();
+        CommandHistory.add(this);
+    }
+
+    /*Get active primary color; save the original color of every selected shape; change selected shape colors; notify
+    * observers to redraw canvas*/
+    private void changePrimaryColor(){
         ShapeColor color  = applicationState.getActivePrimaryColor();
         for(ShapeObject shape : applicationState.getShapeListManager().getSelectedShapeListObject().getList()){
             originalColors.add(shape.getShapePrimaryColor());
             shape.setShapePrimaryColor(color);
         }
         applicationState.getShapeListManager().getShapeListObject().notifyObservers();
-        CommandHistory.add(this);
     }
 
+
     @Override
+    /*iterate over selected shapes and set color to those saved in OriginalColors array*/
     public void undo() {
         for(int i = 0 ; i<applicationState.getShapeListManager().getSelectedShapeListObject().getList().size(); i++){
             ShapeObject shape = applicationState.getShapeListManager().getSelectedShapeListObject().getList().get(i);
@@ -40,6 +48,6 @@ public class ChangePrimaryColorCommand implements ICommand, IUndoable {
 
     @Override
     public void redo() {
-        execute();
+        changePrimaryColor();
     }
 }
--- src/controller/commands/ChangeSecondaryColorCommand.java
+++ src/controller/commands/ChangeSecondaryColorCommand.java
@@ -20,16 +20,24 @@ public class ChangeSecondaryColorCommand implements ICommand, IUndoable {
 
     @Override
     public void execute() {
+        changeSecondaryColor();
+        CommandHistory.add(this);
+    }
+
+    /*Get active secondary color; save the original color of every selected shape; change selected shape colors; notify
+     * observers to redraw canvas*/
+    private void changeSecondaryColor(){
         ShapeColor color  = applicationState.getActiveSecondaryColor();
         for(ShapeObject shape : applicationState.getShapeListManager().getSelectedShapeListObject().getList()){
             originalColors.add(shape.getShapeSecondaryColor());
             shape.setShapeSecondaryColor(color);
         }
         applicationState.getShapeListManager().getShapeListObject().notifyObservers();
-        CommandHistory.add(this);
     }
 
+
     @Override
+    /*iterate over selected shapes and set color to those saved in OriginalColors array*/
     public void undo() {
         for(int i = 0 ; i<applicationState.getShapeListManager().getSelectedShapeListObject().getList().size(); i++){
             ShapeObject shape = applicationState.getShapeListManager().getSelectedShapeListObject().getList().get(i);
